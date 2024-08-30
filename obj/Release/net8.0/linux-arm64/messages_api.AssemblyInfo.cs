49701eed49fba07a6d27e23d009e2bca830b5f5f
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-08-30 22:36:21 +0300

    arm64

--- obj/Release/net8.0/linux-arm64/messages_api.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-arm64/messages_api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("messages_api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606d24a536cd3871c296e8dff4fcb8fd242604a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("messages_api")]
